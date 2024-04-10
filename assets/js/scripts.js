7d866a255c3f9a137c5a9253a2afa81d58ec975f
Kodiak <stephens.ar@gmail.com>
2024-04-10 13:39:22 -0500

    integrated form submit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,21 @@
 // This javascript file is to search for a movie.
+const movieNameInput = document.querySelector('#userInput'); 
+const formSubmit = document.querySelector('.searchForm');
 
-
+console.log(formSubmit);
 const optionsTMBD = {
-	method: 'GET',
+  method: 'GET',
 	headers: {
-	  accept: 'application/json',
+    accept: 'application/json',
 	  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZGFiNmM3NzE0MDZiMDYwNGJhMTViZWNkOTRiMDAwNiIsInN1YiI6IjY2MTU2NzUxOGVlMGE5MDE2NWE0ODFjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iXAYBrRmgumRMOZbdk7mb79G7Wi5iKgl8Xeuj1b343U'
 	}
-  };
+};
 
-  const movieNameInput = 'Inception'; // Placeholder movie until input is made.
 
-  function searchMovie(movie) {
-    fetch(`https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US&page=1`, optionsTMBD)
+function searchMovie(movie) {
+  fetch(`https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US&page=1`, optionsTMBD)
 	.then(response => {
-       return response.json();
+    return response.json();
     })
 	.then(response => {
         console.log(response);
@@ -24,7 +25,13 @@ const optionsTMBD = {
     });
 
   }
+function formHandler(event) {
+  event.preventDefault();
+  console.log('Submitted the form')
+  const inputValue = movieNameInput.value;
+  searchMovie(inputValue);
+}
 
+  formSubmit.addEventListener('submit', formHandler);
   
 
-  searchMovie(movieNameInput);
